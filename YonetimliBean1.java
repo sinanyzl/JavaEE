392a00375db6b786d98c0cbb2f7f99504c22a5bc
Sinan Yozgatlı <75254489+sinanyzl@users.noreply.github.com>
2024-06-14 13:17:33 +0300

    YonetimliBean.java

--- YonetimliBean1.java
+++ YonetimliBean1.java
@@ -1,20 +1,56 @@
-package com.siteadi;
-
+package com.sitenizinadi;
+import java.io.Serializable;
+import java.util.Map;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
-@ManagedBean(name = "yb1")
+@ManagedBean(name = "yb")
 @SessionScoped
-public class YonetimliBean1 {
-
-    public boolean yazilimiSeviyorMu;
+public class YonetimliBean implements Serializable {
     
-    public boolean isYazilimiSeviyorMu() {
-        return yazilimiSeviyorMu;
+    public String adi, soyadi, bolumu;
+    
+    public String getAdi() {
+        return adi;
+    }
+    
+    public void setAdi(String adi) {
+        this.adi = adi;
+    }
+//---------------------------------
+    public String getSoyadi() {
+        return soyadi;
+    }
+    
+    public void setSoyadi(String soyadi) {
+        this.soyadi = soyadi;
+    }
+//---------------------------------
+    public String getBolumu() {
+        return bolumu;
+    }
+    
+    public void setBolumu(String bolumu) {
+        this.bolumu = bolumu;
+    }
+//---------------------------------
+    
+ public String yonelenilecekSayfaGit() {
+
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        this.bolumu = getBolumuParametresiniAl(facesContext);
+
+        return "sayfa2";
     }
 
-    public void setYazilimiSeviyorMu(boolean yazilimiSeviyorMu) {
-        this.yazilimiSeviyorMu = yazilimiSeviyorMu;
+    // "f:param" ile gelen değerler alınıyor.
+    public String getBolumuParametresiniAl(FacesContext facesContext) {
+
+        Map<String, String> params =
+                facesContext.getExternalContext().getRequestParameterMap();
+
+        return params.get("bolumu");
     }
     
 }
